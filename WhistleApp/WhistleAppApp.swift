f19b194e2caa2c58914db7620fc396f670f8625b
massimoyang <massimoyang@tencent.com>
2022-10-27 20:05:44 +0800

    Initial Commit

---
+++ WhistleApp/WhistleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhistleAppApp.swift
+//  WhistleApp
+//
+//  Created by 杨思柏 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct WhistleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
